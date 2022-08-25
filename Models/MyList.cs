509d40966a21fed872d1657278f72f8c7a20d63e
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-25 18:50:14 +0400

    Initial commit

---
+++ Models/MyList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericType.Models
+{
+    class MyList <T>
+    {
+        T[] _arr;
+        private int  _capacity=1;
+
+        public int  Capacity
+        {
+            get { return _capacity=1; }
+            set
+            {
+                if (value>0)
+                {
+                    _capacity = value;
+                    return;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+        private int _count;
+        
+        public int Count { get => _count; }
+        
+        
+        public MyList(int capacity)
+        {
+            
+            Capacity = capacity;
+            _arr = new T[Capacity];
+            
+        }
+
+        
+        public T this[int index] 
+        {
+            get 
+            {
+                if (index<_arr.Length)
+                {
+                    return _arr[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set 
+            {
+                if (index < _arr.Length)
+                {
+                    _arr[index] = value;
+                    return;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public void Add(T value)
+        {
+            if (Count==_arr.Length)
+            {
+                Array.Resize(ref _arr, _arr.Length + Capacity);
+
+            }
+            _count++;
+            _arr[Count - 1] = value;
+            
+            
+            
+            
+        }
+        public void Clear() 
+        {
+            _arr = new T[0];
+            _count = 0;
+        }
+        public bool Exist(T value) 
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(_arr[i],value))
+                {
+                    return true;
+                }
+                
+                
+            }
+            return false;
+        }
+        public void Remove(T value) 
+        {
+            if (IndexOf(value) == -1)
+            {
+                throw new Exception();
+            }
+            for (int i = IndexOf(value); i < _arr.Length; i++)
+            {
+                if (i + 1 != _arr.Length)
+                {
+                    _arr[i] = _arr[i + 1];
+                }
+                else
+                    break;
+            }
+            Array.Resize(ref _arr, _arr.Length - 1);
+            _count--;
+            
+        }
+        public void Reverse() 
+        {
+            T valueFirst;
+            
+            for (int i = 0; i <Count; i++)
+            {
+
+                if (i<Count/2)
+                {
+                    valueFirst = _arr[i];
+                    _arr[i] = _arr[_arr.Length - 1 - i];
+                    _arr[_arr.Length - 1 - i] = valueFirst;   
+                }
+
+            }
+
+        }
+        public int IndexOf(T value) 
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(_arr[i],value))
+                {
+                    return i;
+
+                }
+            }
+            return -1;
+        }
+        public int LastIndexOf(T value) 
+        {
+            int indexOf = -1;
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(_arr[i],value))
+                {
+                    indexOf = i;
+                }
+
+            }
+            return indexOf;
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using GenericType.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GenericType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MyList<int> listNo1 = new MyList<int>(23);
+                listNo1.Add(5);
+                listNo1.Add(6);
+                listNo1.Add(7);
+                listNo1.Add(8);
+                listNo1.Add(9);
+                listNo1.Remove(7);
+                listNo1.Reverse();
+                //Console.WriteLine(listNo1.IndexOf(6));
+                //Console.WriteLine(listNo1.Exist(6));
+                for (int i = 0; i < listNo1.Count; i++)
+                {
+                    Console.WriteLine(listNo1[i]);
+                }
+                MyList<string> listNo2 = new MyList<string>(6);
+                listNo2.Add("Jeyhun");
+                listNo2.Add("Benedict");
+                for (int i = 0; i < listNo2.Count; i++)
+                {
+                    Console.WriteLine(listNo2[i]);
+                }
+                listNo1.Clear();
+
+            }
+            catch (IndexOutOfRangeException) 
+            {
+
+                Console.WriteLine("Number is out of range of array");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Wrong input!");
+            }
+            
+
+        }
+    }
+}
